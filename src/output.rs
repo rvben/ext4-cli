374034dfaf9d5571972046b6de929a9eaf3267a8
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-04-13 13:03:29 +0200

    chore: add rust-version, commit Cargo.lock, clean stub comments

--- src/output.rs
+++ src/output.rs
@@ -1,9 +1,7 @@
 use ext4_view::FileType;
 
 pub fn format_mode(_file_type: FileType, _mode: u16) -> String {
-    String::new() // implemented in Task 4
+    String::new()
 }
 
-pub fn print_json<T: serde::Serialize>(_value: &T) {
-    // implemented in Task 4
-}
+pub fn print_json<T: serde::Serialize>(_value: &T) {}
